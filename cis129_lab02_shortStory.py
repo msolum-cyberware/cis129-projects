2280b194629eaca10f3ef32b13124dd5a727e108
msolum-cyberware <msolum@mail.pima.edu>
2024-09-11 14:55:02 -0700

    Update cis129_lab02_shortStory.py
    The original print line was deleted.
    
    The file's author & behavior were documented in comments.
    
    The program was amended to add a 3-line story about a little dog named "Jill".

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,7 @@
-print('Hello, World!')
+# Author: Martin Solum
+# My first Python lab in my CIS129 course
+# Prints a simple short story
+
+print("There once was a little dog named Jill.")
+print("Everybody loves Jill.")
+print("Jill is the ambassador of JOY!")
